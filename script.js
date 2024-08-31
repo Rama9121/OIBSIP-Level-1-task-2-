36be4ce572ebb89631d9dcaeb14026a4d1f14eab
Sitaram Gandeti <128619172+Rama9121@users.noreply.github.com>
2024-08-31 10:27:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Fresher", "Hardworker", "Web Developer", "Programmer"],
+        strings: [ "Fresher", "Hardworker", "Java Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
